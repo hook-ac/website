57e1d388f3d97966152103b4284898ba12dbe06f
cunev <cunevnew@gmail.com>
2024-04-24 13:24:14 -0700

    feat-profile: define settings

--- src/state.ts
+++ src/state.ts
@@ -1,4 +1,5 @@
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://bogjwtpfbwbchxteoxlo.supabase.co'
-const supabaseKey = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJvZ2p3dHBmYndiY2h4dGVveGxvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM4MjIyNjUsImV4cCI6MjAyOTM5ODI2NX0.sHJfqpD4AvixbYkbM8eBvgM24i9_aweZyBj3MIZyO6o`
-export const supabase = createClient(supabaseUrl, supabaseKey)
+import { createClient } from "@supabase/supabase-js";
+import { Database } from "./supabase";
+const supabaseUrl = "https://bogjwtpfbwbchxteoxlo.supabase.co";
+const supabaseKey = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJvZ2p3dHBmYndiY2h4dGVveGxvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM4MjIyNjUsImV4cCI6MjAyOTM5ODI2NX0.sHJfqpD4AvixbYkbM8eBvgM24i9_aweZyBj3MIZyO6o`;
+export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
